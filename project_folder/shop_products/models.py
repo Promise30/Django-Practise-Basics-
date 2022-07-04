d6e33a9eabcbe76d90535f8c0221ec572be89a81
Promise30 <promiseadeagbo@gmail.com>
2022-07-04 13:04:27 +0100

    Updated the models file to return object created in a readable format

--- project_folder/shop_products/models.py
+++ project_folder/shop_products/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from datetime import datetime
 # Create your models here.
 class Product(models.Model):
-    product_name = models.CharField(max_length=20, unique=True),
+    product_name = models.CharField(max_length=20, default="Unknown name")
     price = models.DecimalField(max_digits=100, decimal_places=2)
     description = models.TextField()
     year_guarantee = models.PositiveSmallIntegerField()
     availability = models.BooleanField()
     published = models.DateField(default=datetime.now)
+
+    def __str__(self):
+        return f'Product Name: {self.product_name}, Price: {self.price}, Description: {self.description}'
