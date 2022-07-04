835d62bcf20d02a6b0b37b445021a109eef19434
Promise30 <promiseadeagbo@gmail.com>
2022-07-04 12:36:38 +0100

    Added the first model called Product to the models file of the app

--- project_folder/shop_products/models.py
+++ project_folder/shop_products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=20, unique=True),
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+    description = models.TextField()
+    year_guarantee = models.PositiveSmallIntegerField()
+    availability = models.BooleanField()
+    published = models.DateField(default=datetime.now)
