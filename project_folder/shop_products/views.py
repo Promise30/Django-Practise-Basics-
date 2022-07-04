69b95786244703157f5f9bc6b8bba0945899fb8c
Promise30 <promiseadeagbo@gmail.com>
2022-07-04 13:55:00 +0100

    Added another view: about.html to the views file

--- project_folder/shop_products/views.py
+++ project_folder/shop_products/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, 'home_html', {});
+    return render(request, 'home.html', {});
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "text": "Our Campany is aimed at providing the best services possible",
+        "info": "Prices are very decent and made affordable for anyone to purchase our products. Some of which include: ",
+        "product_types": ['Perfume: $299.99', 'Balenciaga: $349.99', 'Soft-drinks: $119.99', 'Pasteries: $99.99', 'Chocolates: $499.99']
+    }
+    return render(request, 'about.html', context)
