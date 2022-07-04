6e489851b41c30281af58439d5225648a36e818d
Promise30 <promiseadeagbo@gmail.com>
2022-07-04 14:18:17 +0100

    Added the path to the about_view

--- project_folder/firstProject/urls.py
+++ project_folder/firstProject/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from shop_products.views import home_view
+from shop_products.views import home_view, about_view
 urlpatterns = [
     path('', home_view, name='home'),
+    path('about/' , about_view , name='about'),
     path('admin/', admin.site.urls),
 ]
